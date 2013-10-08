334321fce993c8d0097c5ee0a60f71c5f9193e4f
Brian Galey <bkgaley@gmail.com>
2013-10-08 02:41:15 -0700

    Remove BaseImageIO inheritance and use a single ImageIO class

--- contones/io.py
+++ contones/io.py
@@ -4,52 +4,55 @@ import uuid
 from osgeo import gdal
 import contones.raster
 
+def available_drivers():
+    drivers = {}
+    for i in range(gdal.GetDriverCount()):
+        d = gdal.GetDriver(i)
+        drivers[d.ShortName] = d.GetMetadata()
+        d = None
+    return drivers
 
-# TODO: Generalize and replace _run_encoder()
 def convert(inpath, outpath=None, geom=None):
     if outpath is None:
-        outpath = get_imageio_for(outpath)()
+        outpath = ImageIO()
     with contones.raster.Raster(inpath) as r:
         r.save(outpath)
     return outpath
 
-def get_imageio_for(path):
-    """Returns the io class from a file path or gdal.Driver ShortName."""
-    extsep = os.path.extsep
-    ext = path.rsplit(extsep, 1)[-1] if extsep in path else path
-    #ext = os.path.splitext(path)[-1] if extsep in path else path
-    for cls in BaseImageIO.__subclasses__():
-        if ext in [cls.ext, cls.driver_name]:
-            return cls
-    raise Exception('No IO class for {}'.format(path))
 
-
-# TODO: Work with all GDAL types
-# TODO: These not strictly encoders as they have filepaths, etc. Rename to
-# Transformer, Converter, Driver? Or, FileStore, ImageFile, ImageFileStore?
-#class BaseEncoder(object):
-#class BaseImageStore(object):
-class BaseImageIO(object):
+class ImageIO(object):
     """Base encoder for GDAL Datasets derived from GDAL.Driver, used mainly
     for raster image encoding. New raster formats should subclass this.
     """
     _vsimem = '/vsimem'
-    # Specify this in subclass
-    driver_name = None
-    driver_opts = []
-    ext = None
+    # GDAL driver default creation options.
+    drivers = {'tif': ['COMPRESS=PACKBITS'],
+               'img': ['COMPRESSED=YES']}
 
-    def __init__(self, path=None):
-        self.driver = gdal.GetDriverByName(self.driver_name)
+    def __init__(self, path=None, driver=None):
+        """
+        Keyword args:
+        path -- str GDALDriver name like 'GTiff' or path to a new raster
+        dataset like '/data/test.tif'
+        driver -- GDALDriver instance
+        """
+        if isinstance(driver, str):
+            driver = gdal.GetDriverByName(driver)
+        if driver is None:
+            driver = self.driver_for_path(path)
+        if not isinstance(driver, gdal.Driver):
+            raise Exception('No GDAL driver for {}'.format(path))
+        self.driver = driver
+        self.driver_opts = self.drivers.get(self.ext, [])
         self.path = path or self.get_tmpname()
 
     def __getattr__(self, attr):
         return getattr(self.driver, attr)
 
-    def create(self, nx, ny, bandcount, datatype):
+    def create(self, nx, ny, bandcount, datatype, options=None):
         self._check_exists()
         ds = self.Create(self.path, nx, ny, bandcount,
-                         datatype, self.driver_opts)
+                         datatype, options or self.driver_opts)
         return contones.raster.Raster(ds)
 
     #def vsipath(self):
@@ -61,10 +64,10 @@ class BaseImageIO(object):
         if os.path.exists(self.path):
             raise IOError('{} already exists'.format(self.path))
 
-    def copy_from(self, dataset):
+    def copy_from(self, dataset, options=None):
         #self._check_exists()
         ds = self.CreateCopy(self.path, dataset.ds,
-                             options=self.driver_opts)
+                             options=options or self.driver_opts)
         return contones.raster.Raster(ds)
 
     def read(self, size=0):
@@ -80,52 +83,27 @@ class BaseImageIO(object):
     def unlink(self):
         gdal.Unlink(self.path)
 
-
-#class ImageIO(object):
-class ImageIO(BaseImageIO):
-    # GDAL driver defaults
-    drivers = {'GTiff': ('tif', ['COMPRESS=PACKBITS']),
-               'HFA': ('img', ['COMPRESSED=YES'])}
-
-    def __init__(self, path=None, driver=None):
-        if path and driver is None:
-            driver = self.driver_for_path(path)
-        #if not driver:
-        if not isinstance(driver, gdal.Driver):
-            raise Exception('No GDAL driver for {}'.format(path))
-        self.driver = driver
-        opts = self.drivers.get(self.driver.ShortName)
-        self.driver_opts = opts[1] if opts else []
-        self.path = path or self.get_tmpname()
-        self._ext = None
+    @property
+    def info(self):
+        return self.driver.GetMetadata()
 
     @property
     def ext(self):
-        #k = self.driver.ShortName
-        #self.defaults.get(k, k.lower().replace(' ', ''))
-        try:
-            #self.ext = opts[0]
-            #self.ext = self.driver_opts[0]
-            self._ext = self.drivers[self.driver.ShortName][0]
-        except KeyError:
-            self._ext = self.driver.ShortName.lower().replace(' ', '')
-        return self._ext
+        return self.info.get('DMD_EXTENSION', '')
+
+    @property
+    def mimetype(self):
+        return self.info.get('DMD_MIMETYPE', 'application/octet-stream')
 
     def driver_for_path(self, path):
+        path = path or ''
         extsep = os.path.extsep
-        ext = path.rsplit(extsep, 1)[-1] if extsep in path else path
-        return gdal.GetDriverByName(self.drivers.get(ext, 'GTiff'))
-
-
-class GeoTIFFEncoder(BaseImageIO):
-    """GeoTIFF raster encoder."""
-    driver_name = 'GTiff'
-    driver_opts = ['COMPRESS=PACKBITS']
-    ext = 'tif'
-
-
-class HFAEncoder(BaseImageIO):
-    """Erdas Imagine raster encoder."""
-    driver_name = 'HFA'
-    driver_opts = ['COMPRESSED=YES']
-    ext = 'img'
+        ext = (path.rsplit(extsep, 1)[-1] if extsep in path else path).lower()
+        avail = available_drivers() if ext else {}
+        drivername = 'GTiff'
+        for k, v in avail.items():
+            avail_ext = v.get('DMD_EXTENSION')
+            if ext == avail_ext:
+                drivername = k
+                break
+        return gdal.GetDriverByName(drivername)
