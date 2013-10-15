72b3642953d0e14d4b4c9ec03560a96d259f7d16
Brian Galey <bkgaley@gmail.com>
2013-10-14 21:37:16 -0700

    Remove monkey patching in favor of inheritance for SpatialReference

--- contones/srs.py
+++ contones/srs.py
@@ -2,35 +2,43 @@
 from osgeo import osr
 
 
-# Monkey patch SpatialReference since inheriting from SWIG classes is a hack
-def srid(self):
-    """Returns the EPSG ID as int if it exists."""
-    epsg_id = (self.GetAuthorityCode('PROJCS') or
-                self.GetAuthorityCode('GEOGCS'))
-    try:
-        return int(epsg_id)
-    except TypeError:
-        return
-osr.SpatialReference.srid = property(srid)
+class BaseSpatialReference(osr.SpatialReference):
+    """Base class for extending osr.SpatialReference."""
 
-def wkt(self):
-    """Returns this projection in WKT format."""
-    return self.ExportToWkt()
-osr.SpatialReference.wkt = property(wkt)
+    def __repr__(self):
+        return self.wkt
 
-def proj4(self):
-    """Returns this projection as a proj4 string."""
-    return self.ExportToProj4()
-osr.SpatialReference.proj4 = property(proj4)
+    @property
+    def srid(self):
+        """Returns the EPSG ID as int if it exists."""
+        epsg_id = (self.GetAuthorityCode('PROJCS') or
+                    self.GetAuthorityCode('GEOGCS'))
+        try:
+            return int(epsg_id)
+        except TypeError:
+            return
 
-def __repr__(self): return self.wkt
-osr.SpatialReference.__repr__ = __repr__
+    @property
+    def wkt(self):
+        """Returns this projection in WKT format."""
+        return self.ExportToWkt()
+
+    @property
+    def proj4(self):
+        """Returns this projection as a proj4 string."""
+        return self.ExportToProj4()
 
 
 class SpatialReference(object):
+    """A spatial reference."""
 
     def __new__(cls, sref):
-        sr = osr.SpatialReference()
+        """Returns a new BaseSpatialReference instance
+
+        This allows for customized construction of osr.SpatialReference which
+        has no init method which precludes the use of super().
+        """
+        sr = BaseSpatialReference()
         if isinstance(sref, int):
             sr.ImportFromEPSG(sref)
         elif isinstance(sref, str):
