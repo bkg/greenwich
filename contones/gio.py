0158725a47c9ad4f48be705916f1a4a613156f0b
Brian Galey <bkgaley@gmail.com>
2014-04-07 13:55:49 -0700

    Create ImageDriver class for gdal.Driver handling

--- contones/gio.py
+++ contones/gio.py
@@ -20,49 +20,39 @@ def driver_for_path(path):
     """Returns the gdal.Driver for a path or None based on the file extension.
 
     Arguments:
-    path -- file path as str with a GDAL support file extension
+    path -- file path as str with a GDAL supported file extension
     """
-    path = path or ''
     extsep = os.path.extsep
     ext = (path.rsplit(extsep, 1)[-1] if extsep in path else path).lower()
-    avail = available_drivers() if ext else {}
+    avail = ImageDriver.registry if ext else {}
     for k, v in avail.items():
-        avail_ext = v.get('DMD_EXTENSION')
+        avail_ext = v.get('DMD_EXTENSION', '').lower()
         if ext == avail_ext:
-            return gdal.GetDriverByName(k)
+            return ImageDriver(gdal.GetDriverByName(k))
     return None
 
 
-class ImageIO(object):
-    """File or memory (VSIMEM) backed IO for GDAL datasets.
 
-    GDAL does not integrate with file-like objects but provides its own
-    mechanisms for handling IO.
-    """
-    _vsimem = '/vsimem'
+class ImageDriver(object):
     # GDAL driver default creation options.
-    drivers = {'tif': ['TILED=YES', 'COMPRESS=PACKBITS'],
-               'img': ['COMPRESSED=YES']}
+    defaults = {'tif': ['TILED=YES', 'COMPRESS=PACKBITS'],
+                'img': ['COMPRESSED=YES']}
+    registry = available_drivers()
 
-    def __init__(self, path=None, driver=None):
+    def __init__(self, driver=None):
         """
         Keyword args:
-        path -- str GDALDriver name like 'GTiff' or path to a new raster
-        dataset like '/data/test.tif'
-        driver -- GDALDriver instance
+        driver -- str GDALDriver name like 'GTiff' or GDALDriver instance
         """
         # Use geotiff as the default when path and driver are not provided.
-        if not any((path, driver)):
+        if not driver:
             driver = 'GTiff'
         if isinstance(driver, str):
             driver = gdal.GetDriverByName(driver)
-        if driver is None:
-            driver = driver_for_path(path)
         if not isinstance(driver, gdal.Driver):
-            raise Exception('No GDAL driver for {}'.format(path))
+            raise TypeError('No GDAL driver for {}'.format(driver))
         self._driver = driver
-        self.driver_opts = self.drivers.get(self.ext, [])
-        self.path = path or self._tempname()
+        self.options = self.defaults.get(self.ext, [])
 
     def __getattr__(self, attr):
         return getattr(self._driver, attr)
@@ -70,59 +60,18 @@ class ImageIO(object):
     def __repr__(self):
         return '{}: {}'.format(self.__class__.__name__, str(self.info))
 
-    def is_empty(self):
-        """Returns True if file is empty."""
-        try:
-            return os.path.getsize(self.path) == 0
-        except OSError:
-            # File does not even exist
-            return True
-
-    def create(self, nx, ny, bandcount=1, datatype=gdal.GDT_Byte,
-               options=None):
-        """Returns a new Raster instance.
-
-        gdal.Driver.Create() does not support all formats.
+    def copy(self, source, dest=None, options=None):
+        """Returns a copied Raster instance.
+        Arguments:
+        source -- the source Raster instance
         """
-        # Do not write to a non-empty file.
-        if not self.is_empty():
-            errmsg = '{0} already exists, open with Raster({0})'.format(self.path)
-            raise IOError(errmsg)
-        if nx < 0 or ny < 0:
-            raise ValueError('Size cannot be negative')
-        ds = self.Create(self.path, nx, ny, bandcount,
-                         datatype, options or self.driver_opts)
-        if not ds:
+        dest = ImageIO(dest)
+        if source.name == dest.name:
             raise ValueError(
-                'Could not create {} using {}'.format(self.path, str(self)))
+                'Input and output are the same location: {}'.format(source.name))
+        ds = self.CreateCopy(dest.name, source.ds,
+                             options=options or self.options)
         return contones.raster.Raster(ds)
-
-    def _tempname(self):
-        basename = '{}.{}'.format(str(uuid.uuid4()), self.ext)
-        return os.path.join(self._vsimem, basename)
-
-    def copyfrom(self, dataset, options=None):
-        """Returns a copied Raster instance."""
-        if self.path == dataset.GetDescription():
-            raise ValueError(
-                'Input and output are the same location: {}'.format(self.path))
-        ds = self.CreateCopy(self.path, dataset.ds,
-                             options=options or self.driver_opts)
-        return contones.raster.Raster(ds)
-
-    def getvalue(self):
-        """Returns the raster data buffer as a byte string."""
-        f = gdal.VSIFOpenL(self.path, 'rb')
-        if f is None:
-            raise IOError('Could not read from {}'.format(self.path))
-        fstat = gdal.VSIStatL(self.path)
-        data = gdal.VSIFReadL(1, fstat.size, f)
-        gdal.VSIFCloseL(f)
-        return data
-
-    def unlink(self):
-        """Delete the file or vsimem path."""
-        gdal.Unlink(self.path)
 
     @property
     def info(self):
@@ -138,3 +87,78 @@ class ImageIO(object):
     def mimetype(self):
         """Returns the MIME type."""
         return self.info.get('DMD_MIMETYPE', 'application/octet-stream')
+
+
+class ImageIO(object):
+    """File or memory (VSIMEM) backed IO for GDAL datasets.
+
+    GDAL does not integrate with file-like objects but provides its own
+    mechanisms for handling IO.
+
+    path -- path to a raster dataset like '/tmp/test.tif'
+    driver -- name as string like 'JPEG' or a gdal.Driver instance
+    """
+    _vsimem = '/vsimem'
+
+    def __init__(self, path=None, driver=None):
+        path = getattr(path, 'name', path)
+        if path:
+            self.driver = driver_for_path(path)
+            self.name = path
+        else:
+            self.driver = ImageDriver(driver)
+            self.name = self._tempname()
+
+    #def create_raster
+    def create(self, shape, datatype=gdal.GDT_Byte, options=None):
+        """Returns a new Raster instance.
+
+        gdal.Driver.Create() does not support all formats.
+
+        Arguments:
+        shape -- two or three-tuple of (xsize, ysize, bandcount)
+        datatype -- GDAL pixel data type
+        options -- dict of dataset creation options
+        """
+        if len(shape) == 2:
+            shape += (1,)
+        nx, ny, bandcount = shape
+        if nx < 0 or ny < 0:
+            raise ValueError('Size cannot be negative')
+        # Do not write to a non-empty file.
+        if not self._is_empty():
+            errmsg = '{0} already exists, open with Raster({0})'.format(self.name)
+            raise IOError(errmsg)
+        ds = self.driver.Create(self.name, nx, ny, bandcount, datatype,
+                                options or self.driver.options)
+        if not ds:
+            raise ValueError(
+                'Could not create {} using {}'.format(self.name, str(self)))
+        return contones.raster.Raster(ds)
+
+    def getvalue(self):
+        """Returns the raster data buffer as a byte string."""
+        f = gdal.VSIFOpenL(self.name, 'rb')
+        if f is None:
+            raise IOError('Could not read from {}'.format(self.name))
+        fstat = gdal.VSIStatL(self.name)
+        data = gdal.VSIFReadL(1, fstat.size, f)
+        gdal.VSIFCloseL(f)
+        return data
+
+    def _is_empty(self):
+        """Returns True if file is empty or non-existent."""
+        try:
+            return os.path.getsize(self.name) == 0
+        except OSError:
+            # File does not even exist
+            return True
+
+    def _tempname(self):
+        """Returns a temporary VSI memory filename."""
+        basename = '{}.{}'.format(str(uuid.uuid4()), self.driver.ext)
+        return os.path.join(self._vsimem, basename)
+
+    def unlink(self):
+        """Delete the file or vsimem path."""
+        gdal.Unlink(self.name)
--- contones/test/test_gio.py
+++ contones/test/test_gio.py
@@ -1,39 +1,71 @@
 import tempfile
 
+from osgeo import gdal
+
 from .test_raster import RasterTestBase
 from contones.raster import Raster
-from contones.gio import ImageIO, driver_for_path
+from contones.gio import driver_for_path, ImageDriver, ImageIO
 
 
-class ImageIOTestCase(RasterTestBase):
-    def test_getvalue(self):
-        """Test ImageIO reading."""
-        for dname in 'GTiff', 'HFA':
-            io_obj = ImageIO(driver=dname)
-            with Raster(self.f.name) as r:
-                r.save(io_obj)
-            data = io_obj.getvalue()
-            self.assertIsNotNone(data)
-            self.assertGreater(data, 0)
+class ImageDriverTestCase(RasterTestBase):
 
     def test_copy_from(self):
-        ds_copy = ImageIO(driver='PNG').copyfrom(self.ds)
+        ds_copy = ImageDriver('PNG').copy(self.ds)
         self.assertIsInstance(ds_copy, Raster)
         self.assertEqual(ds_copy.io.ext, 'png')
         # Make sure we get the same number of raster bands back.
         self.assertEqual(*map(len, (self.ds, ds_copy)))
 
+    def test_driver_for_path(self):
+        self.assertEqual(driver_for_path('test.jpg').ShortName, 'JPEG')
+        self.assertEqual(driver_for_path('test.zzyyxx'), None)
+
+    def test_init(self):
+        self.assertEqual(ImageDriver().ShortName, 'GTiff')
+        img = ImageDriver('HFA')
+        self.assertEqual(img.ShortName, 'HFA')
+        self.assertEqual(img.ext, 'img')
+        hdriver = gdal.GetDriverByName('HFA')
+        self.assertEqual(ImageDriver(hdriver)._driver, hdriver)
+
+
+class ImageIOTestCase(RasterTestBase):
+
     def test_create(self):
         f = tempfile.NamedTemporaryFile(suffix='.img')
-        imgio = ImageIO(f.name)
-        with self.assertRaises(ValueError):
-            r = imgio.create(-10, -10)
+        imgio = ImageIO(f)
+        self.assertRaises(ValueError, imgio.create, ((-10, -10),))
         size = (10, 10)
-        rast = imgio.create(*size)
+        rast = imgio.create(size)
         self.assertEqual(rast.shape, size)
         self.assertEqual(rast.io.ext, 'img')
         f.close()
 
-    def test_driver_for_path(self):
-        self.assertEqual(driver_for_path('test.jpg').ShortName, 'JPEG')
-        self.assertEqual(driver_for_path('test.zzyyxx'), None)
+    def test_create_options(self):
+        opts = ['TILED=YES', 'COMPRESS=DEFLATE']
+        imgio = ImageIO(driver='GTiff')
+        rast = imgio.create((10, 10), options=opts)
+        # We cannot verify metadata from an open GDALDataset, it must be
+        # reopened first.
+        rast.close()
+        rast = Raster(imgio.name)
+        # The compression name is changed slightly within the GDAL Dataset.
+        expected_opt = 'COMPRESSION=DEFLATE'
+        self.assertIn(expected_opt, rast.ds.GetMetadata_List('IMAGE_STRUCTURE'))
+
+    def test_getvalue(self):
+        """Test ImageIO reading."""
+        io_obj = ImageIO(self.f)
+        data = io_obj.getvalue()
+        self.assertIsInstance(data, str)
+        self.assertGreater(data, 0)
+        io_obj = ImageIO()
+        self.assertRaises(IOError, io_obj.getvalue)
+
+    def test_init(self):
+        self.assertTrue(ImageIO())
+        jpeg = 'test.jpg'
+        rf = ImageIO(jpeg)
+        self.assertEqual(rf.name, jpeg)
+        rf2 = ImageIO(driver='JPEG')
+        self.assertEqual(rf.driver.info, rf2.driver.info)
