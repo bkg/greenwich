096bd0d82e34df6d24fd5069b8840f7e5afd506a
Brian Galey <bkgaley@gmail.com>
2014-05-02 17:12:01 -0700

    Add packaging requirements

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='greenwich',
+      version='0.1.0',
+      description='A GDAL wrapper with Python conveniences',
+      long_description=open('README.rst').read(),
+      author='Brian Galey',
+      author_email='bkgaley@gmail.com',
+      url='https://github.com/bkg/greenwich',
+      packages=['greenwich'],
+      install_requires=['GDAL', 'numpy', 'Pillow'],
+      license='BSD',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+      ],
+      test_suite='tests')
