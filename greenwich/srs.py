e75fe96881a686de45955536878c6fde5314db46
Brian Galey <bkgaley@gmail.com>
2014-05-21 18:48:59 -0700

    No need for mucking with __new__() here

--- greenwich/srs.py
+++ greenwich/srs.py
@@ -1,11 +1,21 @@
 """Spatial reference systems"""
-__all__ = ['SpatialReference']
-
 from osgeo import osr
 
 
-class BaseSpatialReference(osr.SpatialReference):
+class SpatialReference(osr.SpatialReference):
     """Base class for extending osr.SpatialReference."""
+
+    def __init__(self, sref=None):
+        super(SpatialReference, self).__init__()
+        if isinstance(sref, int):
+            self.ImportFromEPSG(sref)
+        elif isinstance(sref, str):
+            if sref.strip().startswith('+proj='):
+                self.ImportFromProj4(sref)
+            else:
+                self.ImportFromWkt(sref)
+            # Add EPSG authority if applicable
+            self.AutoIdentifyEPSG()
 
     def __repr__(self):
         return self.wkt
@@ -20,7 +30,7 @@ class BaseSpatialReference(osr.SpatialReference):
     def srid(self):
         """Returns the EPSG ID as int if it exists."""
         epsg_id = (self.GetAuthorityCode('PROJCS') or
-                    self.GetAuthorityCode('GEOGCS'))
+                   self.GetAuthorityCode('GEOGCS'))
         try:
             return int(epsg_id)
         except TypeError:
@@ -35,28 +45,3 @@ class BaseSpatialReference(osr.SpatialReference):
     def proj4(self):
         """Returns this projection as a proj4 string."""
         return self.ExportToProj4()
-
-
-class SpatialReference(object):
-    """A spatial reference."""
-
-    def __new__(cls, sref):
-        """Returns a new BaseSpatialReference instance
-
-        This allows for customized construction of osr.SpatialReference which
-        has no init method which precludes the use of super().
-        """
-        sr = BaseSpatialReference()
-        if isinstance(sref, int):
-            sr.ImportFromEPSG(sref)
-        elif isinstance(sref, str):
-            if sref.strip().startswith('+proj='):
-                sr.ImportFromProj4(sref)
-            else:
-                sr.ImportFromWkt(sref)
-            # Add EPSG authority if applicable
-            sr.AutoIdentifyEPSG()
-        else:
-            raise TypeError('Cannot create SpatialReference '
-                            'from {}'.format(str(sref)))
-        return sr
