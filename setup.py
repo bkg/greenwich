c3751a10761210f95bd41b7124de7d096dd948e3
Brian Galey <bkgaley@gmail.com>
2014-06-23 16:34:31 -0700

    Not supporting 3.x quite yet

--- setup.py
+++ setup.py
@@ -23,7 +23,5 @@ setup(name='greenwich',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
       ],
       test_suite='tests')
