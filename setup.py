37bfa825e88fe2a1d534ddbcca0a31302dc1318d
Brian Galey <bkgaley@gmail.com>
2015-08-12 10:20:01 -0700

    Release version 0.4.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='greenwich',
-      version='0.4.1',
+      version='0.4.2',
       description='A GDAL wrapper with Python conveniences',
       long_description=open('README.rst').read(),
       author='Brian Galey',
