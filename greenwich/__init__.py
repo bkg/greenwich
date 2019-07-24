fb49d44cd1cb8ea8a3d291d79546914f15a58491
Brian Galey <bkgaley@gmail.com>
2019-07-23 18:48:02 -0700

    Add fromarray to package root

--- greenwich/__init__.py
+++ greenwich/__init__.py
@@ -1,4 +1,4 @@
-from greenwich.raster import (driver_for_path, frombytes, geom_to_array, open,
-    AffineTransform, ImageDriver, Raster)
+from greenwich.raster import (driver_for_path, fromarray, frombytes,
+    geom_to_array, open, AffineTransform, ImageDriver, Raster)
 from greenwich.geometry import Envelope, Geometry
 from greenwich.srs import SpatialReference
